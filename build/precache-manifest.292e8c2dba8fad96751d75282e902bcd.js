ec4044aac6dc43853d86576e087386fbbc370c26
jasurn <97jasur@gmail.com>
2020-10-02 18:14:24 +0500

    add compiled files due to memory overflow on server

---
+++ build/precache-manifest.292e8c2dba8fad96751d75282e902bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59f187601a436b16265442fd88c7e96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db0fc579b4c39786bb49",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "a38caad705994732f828",
+    "url": "/static/css/main.4e5d8f35.chunk.css"
+  },
+  {
+    "revision": "db0fc579b4c39786bb49",
+    "url": "/static/js/2.7c3eba97.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.7c3eba97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38caad705994732f828",
+    "url": "/static/js/main.5be81f06.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
